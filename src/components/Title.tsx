8ffb23ea465658ae8e951cc663eca4e880cb189d
Luis Martins <luismwolf@gmail.com>
2024-04-19 10:18:46 -0400

    make title return to home on click

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,19 +1,18 @@
+import { Link } from "react-router-dom";
+
 interface Params {
   color?: "yellow" | "slate";
   className?: string;
 }
 export const Title = ({ color = "yellow", className = "" }: Params) => (
   <span className={`flex gap-3 text-xl ${className}`}>
-    <p
-      className={
-        color === "slate"
-          ? "text-slate-700 dark:text-slate-300"
-          : "text-yellow-300"
-      }
-    >
+    <Link
+      className={color === "slate"
+        ? "text-slate-700 dark:text-slate-300"
+        : "text-yellow-300"} to={"/"}    >
       {" "}
       Le Gros Chef
-    </p>
+    </Link>
     <p>⏲️</p>
   </span>
 );
